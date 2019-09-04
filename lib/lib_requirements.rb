f9ad43a76c9030b2fa61e2f181607fd6a401b8d3
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-04 16:07:08 -0700

    all helper methods tested w/ 100% coverage

--- lib/lib_requirements.rb
+++ lib/lib_requirements.rb
@@ -3,30 +3,32 @@ require 'time'
 # any other requirements?  idk...
 
 
-module Validation
+module Helpers
   
   def non_zero_integer?(num)
     if num.class != Integer 
-      raise ArgumentError, "#{num} must be an Integer"
+      return false
     elsif num.nil? || num <= 0
-      raise ArgumentError, '#{num} cannot be blank or less than zero.'
+      return false
     end
     return true
   end
   
   def non_blank_string?(str)
     if str.class != String
-      raise ArgumentError, '#{str} must be a String'
+      return false
     elsif str == ""
-      raise ArgumentError, '#{str} cannot be blank '
+      return false
     end
     return true
   end
   
   def available_id?(id, array_taken_ids)
-    non_zero_integer? id
-    (array_taken_ids.include? id)? false : true 
+    if non_zero_integer? id
+      (array_taken_ids.include? id)? false : true 
+    else 
+      return false
+    end
   end
-  
-  
+
 end
---
+++ test/lib_requirements_test.rb
@@ -0,0 +1,36 @@
+require_relative 'test_helpers.rb'
+
+describe "### HELPERS MODULE ###" do
+  before do
+    class SampleClass < Object
+      include Helpers
+    end
+    @sampleClass = SampleClass.new
+  end
+
+  
+  it "Does non_zero_integer? work" do
+    assert(@sampleClass.non_zero_integer? 3)
+
+    bad_args = ["", 0, {}, [], 6.66, Object.new]
+    bad_args.each { |bad_arg| 
+      refute(@sampleClass.non_zero_integer?(bad_arg))
+    }
+  end
+
+  it "Does non-blank_string? work" do
+    assert(@sampleClass.non_blank_string? "xxx")
+    
+    bad_args = ["", 0, {}, [], 6.66, Object.new]
+    bad_args.each { |bad_arg| 
+      refute(@sampleClass.non_blank_string?(bad_arg))
+    }
+  end
+
+  it "Does available_id? work" do
+    assert (@sampleClass.available_id?(4, [1,2,3]))
+
+    refute (@sampleClass.available_id?(1, [1,2,3]))
+    refute (@sampleClass.available_id?("X", [1,2,3]))
+  end
+end
