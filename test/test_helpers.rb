fcecac2b8cc282a6096ba0e8e6a91f8476d78681
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-04 16:04:27 -0700

    fixed simpleCov issue, it needs to go on top haha

---
+++ test/test_helpers.rb
@@ -0,0 +1,29 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/'
+end
+
+require 'pry'   ### deactivate later???
+
+require "minitest"
+require "minitest/autorun"
+require "minitest/reporters"
+
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# require_relative your lib files here!
+require_relative '../lib/lib_requirements.rb'
+require_relative '../lib/room.rb'
+require_relative '../lib/hotel_front_desk.rb'
+
+# require_relative '../lib/block.rb'
+# require_relative '../lib/date.rb'
+# require_relative '../lib/date_range.rb'
+# require_relative '../lib/reservation.rb'
+
+
+
+### DELETE???
+# require_relative '../lib/customer.rb'
