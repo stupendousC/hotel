5a194c2f8bd9030ccc8f0b8e12f67c0136f32499
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-04 16:06:38 -0700

    Revert "all helper"
    This reverts commit d4f01e616666657b16c381dbdcb3603e9ab20a94.

--- lib/lib_requirements.rb
+++ lib/lib_requirements.rb
@@ -3,32 +3,30 @@ require 'time'
 # any other requirements?  idk...
 
 
-module Helpers
+module Validation
   
   def non_zero_integer?(num)
     if num.class != Integer 
-      return false
+      raise ArgumentError, "#{num} must be an Integer"
     elsif num.nil? || num <= 0
-      return false
+      raise ArgumentError, '#{num} cannot be blank or less than zero.'
     end
     return true
   end
   
   def non_blank_string?(str)
     if str.class != String
-      return false
+      raise ArgumentError, '#{str} must be a String'
     elsif str == ""
-      return false
+      raise ArgumentError, '#{str} cannot be blank '
     end
     return true
   end
   
   def available_id?(id, array_taken_ids)
-    if non_zero_integer? id
-      (array_taken_ids.include? id)? false : true 
-    else 
-      return false
-    end
+    non_zero_integer? id
+    (array_taken_ids.include? id)? false : true 
   end
-
+  
+  
 end
