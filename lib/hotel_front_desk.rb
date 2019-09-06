85b0c603b31e9abf2e4c99879228a24b7208e340
stupendousC <CarolineWuKaplan@gmail.com>
2019-09-06 12:36:27 -0700

    hotel_front_desk#make_reseration tested with 95% coverage, the other 5% is redundant validation checks but I'm gonna leave it in as a safety net

--- lib/hotel_front_desk.rb
+++ lib/hotel_front_desk.rb
@@ -6,11 +6,11 @@ require_relative 'date_range'
 class Hotel_front_desk
   include Helpers
   attr_reader :all_rooms, :all_reservations, :num_rooms_in_hotel
-
+  
   def initialize (num_rooms_in_hotel: 20, all_rooms: [], all_reservations: [])
     @all_rooms = all_rooms
     @all_reservations = all_reservations
-
+    
     # set up Room instances
     @num_rooms_in_hotel = num_rooms_in_hotel
     @num_rooms_in_hotel.times do |i|
@@ -18,32 +18,51 @@ class Hotel_front_desk
       @all_rooms << room
     end
   end
-
+  
   def list_all_rooms
     list = "LIST OF ALL ROOMS:\n"
     @all_rooms.each { |room| list << "  ROOM ##{room.id}\n" }
     return list
   end
-
-  def make_reservation(date_range:, customer:)
-    # args validation: no need for customer except just a non-empty string for now
+  
+  def make_reservation(date_range:, customer:, new_nightly_rate: nil)
     if date_range.class != Date_range
       raise ArgumentError, "Requires a Date_range object"
-    elsif  (customer == "") 
+
+    
+    elsif  (customer == "") || (customer == nil)
       raise ArgumentError, "Customer must have a name..."
     elsif (customer.class != String)
       raise ArgumentError, "We don't take kindly to non-String customers around here!"
+      
+    elsif new_nightly_rate
+      if new_nightly_rate.class != Integer
+        raise ArgumentError, "New_nightly_rate must be an Integer"
+      elsif !(non_zero_integer? new_nightly_rate)
+        raise ArgumentError, "New_nightly_rate must be a non-zero Integer"
+      end
+      # I chose to allow new_nightly_rate to be higher number than standard rate
     end
-
+    
     # go thru @all_rooms, result is either a room or nil
     room = find_avail_room(date_range)
-    return nil if room == nil
-
+    if room == nil
+      return nil
+    end
+    
+    # By now, all args have passed validation tests
+    reservation = Reservation.new(room_id: room.id, room: room, date_range: date_range, customer: customer, new_nightly_rate: new_nightly_rate)
+    
     # update Room w/ make_unavail, add to Room.reservations
-
+    room.make_unavail(date_range)
+    room.all_reservations << reservation
+    
     # update @all_reservations
+    @all_reservations << reservation
+    
+    return reservation
   end
-
+  
   def find_avail_room(date_range)
     # returns Room object that is unoccupied on date_range, or nil if no rooms
     @all_rooms.each do |room|
@@ -51,15 +70,15 @@ class Hotel_front_desk
     end
     return nil
   end
-
-
+  
+  
   def calc_cost(*arg)
     # I think i'm going to make reservatin do calc_cost
   end
-
+  
   def list_reservation(date)
     # go thru @all_reservations
     # compare date to each one's start-date & end_date, add to results[] if inclusive
   end
-
+  
 end
--- test/hotel_front_desk_test.rb
+++ test/hotel_front_desk_test.rb
@@ -1,26 +1,26 @@
 require_relative 'test_helpers.rb'
 
 describe "### HOTEL_FRONT_DESK CLASS ###" do
-
+  
   describe "Hotel_front_desk.new" do 
-
+    
     let (:hotel1) { Hotel_front_desk.new }
-
+    
     it "Can create Hotel_front_desk instance " do
       expect(hotel1).must_be_kind_of Hotel_front_desk
     end
-
+    
     it "Should have 20 Room objs made, and stored in @all_rooms array" do
       expect(hotel1.all_rooms.length).must_equal 20
       expect(hotel1.all_rooms).must_be_kind_of Array
     end
-
+    
     it "Should have default @all_reservations of []" do
       expect(hotel1.all_reservations.length).must_equal 0
     end
-
+    
   end
-
+  
   describe "Does .list_all_rooms work?" do
     it "Returned string should show all Room instances" do
       hotel2 = Hotel_front_desk.new(num_rooms_in_hotel: 3)
@@ -29,70 +29,97 @@ describe "### HOTEL_FRONT_DESK CLASS ###" do
       assert(manual_str == print_out)
     end
   end
-
   
-
-
+  
+  
+  
   describe "Does .make_reservation work?" do
     let (:hotel) { Hotel_front_desk.new }
     let (:empty_hotel) { Hotel_front_desk.new(num_rooms_in_hotel:0) }
     let (:today) { Date.today }
     let (:range1) { Date_range.new(start_date_obj: today, end_date_obj: today+2) }
- 
+    
     it "Raises error at args validation step" do
       expect{hotel.make_reservation( date_range: "garbage", customer: "trump" )}.must_raise ArgumentError
+      # bad customer arg
       expect{hotel.make_reservation( date_range: range1, customer: "" )}.must_raise ArgumentError
       expect{hotel.make_reservation( date_range: range1, customer: 666 )}.must_raise ArgumentError
+      expect{hotel.make_reservation( date_range: range1, customer: nil )}.must_raise ArgumentError
+      # bad new_nightly_rate arg
+      expect{hotel.make_reservation( date_range: range1, customer: "garbage", new_nightly_rate: "garbage")}.must_raise ArgumentError
+      expect{hotel.make_reservation( date_range: range1, customer: "garbage", new_nightly_rate: 0.01)}.must_raise ArgumentError
+      expect{hotel.make_reservation( date_range: range1, customer: "garbage", new_nightly_rate: -1)}.must_raise ArgumentError
     end
-
+    
     describe "Does .find_avail_room work?" do
       it "Returns Room obj if a room is available" do
         should_be_a_room = hotel.find_avail_room(range1)
         assert(should_be_a_room.class == Room)
       end
-
+      
       it "Returns nil if none available" do 
         should_be_nil = empty_hotel.find_avail_room(range1)
         assert(should_be_nil == nil)
       end
     end
-
+    
     it "Returns nil if no room available" do
       # just making sure nil from .find_avail_room gets carried to .make_reservation
       should_be_nil = empty_hotel.make_reservation(date_range: range1, customer: "ghost")
       assert(should_be_nil == nil)
     end
-
+    
     describe "Once room is found, proceed with correct behavior" do 
-
+      let (:hotel) { Hotel_front_desk.new }
+      let (:today) { Date.today }
+      let (:range1) { Date_range.new(start_date_obj: today, end_date_obj: today+2) }
+      let (:res1) { hotel.make_reservation(date_range: range1, customer: "Farnsworth", new_nightly_rate: 100) }
+      
       it "Makes new Reservation obj with correct attribs assigned" do
+        expect(res1).must_be_kind_of Reservation
+        
+        expected_new_id = Reservation.class_variable_get(:@@available_id)
+        assert(res1.id == expected_new_id)
+        assert(res1.date_range == range1)
+        assert(res1.start_date == today)
+        assert(res1.end_date == today+2)
+        assert(res1.customer == "Farnsworth")
+        assert(res1.new_nightly_rate == 100)  
       end
-
+      
+      it "Updates RoomObj.occupied_nights via make_unavail, and added ReservationObj to Room.all_reservations" do
+        room = res1.room
+        expect(room).must_be_kind_of Room
+        
+        assert(room.all_reservations.length == 1)
+        assert(room.all_reservations.include? res1)
+      end
+      
       it "Updates Hotel obj's @all_reservations" do
+        assert(hotel.all_reservations == [])
+        res1
+        assert(hotel.all_reservations.length == 1)
+        assert(hotel.all_reservations[0] == res1)
       end
-
-      it "Updates Room obj via make_unavail, also add to Room.all_reservations" do
-      end
-
     end
-
-
-
-
+    
+    
+    
+    
   end
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
   describe "Does .calc_cost work?" do
     # edge: invalid arg
   end
-
+  
   describe "Does .list_reservation work?" do
     # edge: invalid date
   end
-
+  
 end
